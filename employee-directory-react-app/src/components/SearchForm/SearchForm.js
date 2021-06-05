f12e3fbbdd7cade31ac95f6cd80cf9d7e260b04a
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-05 15:50:22 -0400

    created the search component

--- employee-directory-react-app/src/components/SearchForm/SearchForm.js
+++ employee-directory-react-app/src/components/SearchForm/SearchForm.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./SearchForm.css";
 function SearchForm (props) {
     return (
         <form className = "search mx-auto">
             <div className="my-4">
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Search" />
+                <input value={props.search}  onChange={props.handleInputChange} className="form-control" id="searchInput" placeholder="Search" />
             </div>
         </form>
 
