f68a2b44571e7a36d9bb1b57fc282b39e99870e9
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-05 19:52:30 -0400

    adjusted package.json for deployment and added bootstrap icon for filtration with its CDN

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -9,7 +9,7 @@ function SearchResults(props) {
     <thead>
   <tr>
     <th scope="col">Image</th>
-    <th scope="col" onClick={props.handleSortChange}>Name {props.sortAsc && <span>(Asc)</span>} {!props.sortAsc && <span>(Desc)</span>} </th>
+    <th scope="col" onClick={props.handleSortChange}>Name {props.sortAsc && <span><i class="bi bi-sort-alpha-down"></i></span>} {!props.sortAsc && <span><i class="bi bi-sort-alpha-down-alt"></i></span>} </th>
     <th scope="col">Phone</th>
     <th scope="col">Email</th>
     <th scope="col">DOB</th>
