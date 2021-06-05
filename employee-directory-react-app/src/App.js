c42eb6d74b9307d69f61edd6bf2fd67e3d756fe5
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-05 15:55:36 -0400

    did the functions in the APP.js for the search and filtering functionality

--- employee-directory-react-app/src/App.js
+++ employee-directory-react-app/src/App.js
@@ -1,17 +1,108 @@
+import React, { Component } from "react";
 import Header from "./components/header/Header";
-import "./components/header/Header.css";
-
+import API from "./utils/API";
 import SearchForm from "./components/SearchForm/SearchForm";
-import "./components/SearchForm/SearchForm.css";
+import SearchResults from "./components/SearchResults/SearchResults";
+import Moment from 'moment';
 
-function App() {
+class App extends Component {
+  state = {
+    search: "",
+    results: [],
+    filteredresults:[],
+    sortAsc:false,
+    error: ""
+  };
+
+  componentDidMount() {
+    API.getEmployees()
+      .then(res => {
+        this.setState({ results: res.data.results.map(data=>{return {Image:data.picture.medium, Name:data.name.first+' '+data.name.last,Phone:data.phone,Email:data.email, DOB:Moment(data.dob.date).format('MM-DD-YYYY')  }}) });
+        this.setState({ filteredresults: res.data.results.map(data=>{return {Image:data.picture.medium, Name:data.name.first+' '+data.name.last,Phone:data.phone,Email:data.email, DOB: Moment(data.dob.date).format('MM-DD-YYYY') }}).sort(function(a, b) {
+          if(a.Name.toLowerCase() < b.Name.toLowerCase()) return -1;
+          if(a.Name.toLowerCase() > b.Name.toLowerCase()) return 1;
+          return 0;
+         }) });
+      }
+      )
+      .catch(err => console.log(err));
+  }
+  handleInputChange = event => {
+
+    if(this.state.sortAsc)
+    {
+      this.setState({filteredresults:this.state.filteredresults.sort(function(a, b) {
+        if(a.Name.toLowerCase() < b.Name.toLowerCase()) return -1;
+        if(a.Name.toLowerCase() > b.Name.toLowerCase()) return 1;
+        return 0;
+       })});
+    }
+    else
+    {
+      this.setState({filteredresults:this.state.filteredresults.sort(function(a, b) {
+        if(a.Name.toLowerCase() > b.Name.toLowerCase()) return -1;
+        if(a.Name.toLowerCase() < b.Name.toLowerCase()) return 1;
+        return 0;
+       })});
+    }
+    if(this.state.sortAsc)
+    {
+    this.setState({filteredresults: this.state.results.filter(person => person.Name.toLowerCase().includes(event.target.value.toLowerCase())).sort(function(a, b) {
+        if(a.Name.toLowerCase() > b.Name.toLowerCase()) return -1;
+        if(a.Name.toLowerCase() < b.Name.toLowerCase()) return 1;
+        return 0;
+       })
+    
+    });
+    }
+    else
+    {
+      this.setState({filteredresults: this.state.results.filter(person => person.Name.toLowerCase().includes(event.target.value.toLowerCase()))
+        .sort(function(a, b) {
+          if(a.Name.toLowerCase() < b.Name.toLowerCase()) return -1;
+          if(a.Name.toLowerCase() > b.Name.toLowerCase()) return 1;
+          return 0;
+         })
+      });
+   
+    }
+  };
+
+  handleSortChange = event => {
+    // this.setState({ search: event.target.value });
+    this.setState({sortAsc: !this.state.sortAsc});
+    if(this.state.sortAsc)
+    {
+      this.setState({filteredresults:this.state.filteredresults.sort(function(a, b) {
+        if(a.Name.toLowerCase() < b.Name.toLowerCase()) return -1;
+        if(a.Name.toLowerCase() > b.Name.toLowerCase()) return 1;
+        return 0;
+       })});
+    }
+    else
+    {
+      this.setState({filteredresults:this.state.filteredresults.sort(function(a, b) {
+        if(a.Name.toLowerCase() > b.Name.toLowerCase()) return -1;
+        if(a.Name.toLowerCase() < b.Name.toLowerCase()) return 1;
+        return 0;
+       })});
+    }
+     
+   };
+ 
+
+  render() {
   return (
     <div>
     <Header />
-    <SearchForm />
+    <SearchForm   
+            handleInputChange={this.handleInputChange}
+           />
+             <SearchResults handleSortChange={this.handleSortChange} results={this.state.filteredresults} />
     </div>
   );
 
+}
 }
 
 export default App;
