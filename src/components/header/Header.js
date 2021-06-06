e0cb8a2beafc7a09aca23807ce9aa0928c3d3836
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-06 19:43:03 -0400

    changed the paragraph in the header to direct the user about the sorting

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ function Header () {
     return (
         <div className = "header py-5">
             <h1>Employee Directory</h1>
-            <p>Click on carrots to filter heading or use the search box to narrow your results</p>
+            <p>Click on the <span><i class="bi bi-sort-alpha-down"></i></span> or the <span><i class="bi bi-sort-alpha-down-alt"></i></span> to filter the employees names</p>
         </div>
 
     );
